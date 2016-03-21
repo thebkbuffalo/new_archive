097c1f9757ca5dbf1a672532da2a02c47c80827f
Evan Berg <evan.m.berg@gmail.com>
2016-03-20 23:51:04 -0700

    added some stuff

--- gh_archive.rb
+++ gh_archive.rb
@@ -1,8 +1,9 @@
-class Archiver
+class Archive
   require 'open-uri'
   require 'zlib'
   require 'yajl'
   require 'JSON'
+  require 'google-api-client'
   require 'bigquery'
 
   # gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
@@ -14,8 +15,23 @@ class Archiver
   # end
 
   def initialize
-    puts 'GitHub Archiver Challenge"'
+    puts 'GitHub Archiver Challenge'
+    get_input
   end
 
-  
+  def get_input
+    puts 'Find the most active repo in a given time frame'
+    puts 'example:'
+    puts '2012-11-01, 2012-11-02, PushEvent, 42'
+    input = gets.chomp.delete(' ').split(',')
+  end
+
+  def get_data
+    gh = open('http://data.githubarchive.org')
+    js = Zlib::GzipReader.new(gh).read
+  end
+
+
 end #ends class
+
+Archive.new
