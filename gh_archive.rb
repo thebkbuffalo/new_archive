d8d2ce6d120f55692de018c606f78bc9d6226e91
Evan Berg <evan.m.berg@gmail.com>
2016-03-21 10:44:59 -0700

    require bigquery not throwing an error, now to set it up

--- gh_archive.rb
+++ gh_archive.rb
@@ -7,7 +7,7 @@ class Archive
   require 'google/api_client'
   require 'google/api_client/client_secrets'
   require 'google/api_client/auth/installed_app'
-  # require 'bigquery'
+  require 'big_query'
 
   # gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
   # js = Zlib::GzipReader.new(gz).read
@@ -27,17 +27,18 @@ class Archive
     puts 'example:'
     puts '2012-11-01 13:00:00, 2012-11-02 15:12:14, PushEvent, 42'
     input = gets.chomp.delete(' ').split(',')
-    @params = []
-    input.each {|x| @params.push(x)}
-    puts @params[0]
-    puts @params[1]
-    puts @params[2]
-    puts @params[3]
+    params = []
+    input.each {|x| params.push(x)}
+    @start = params[0]
+    @end = params[1]
+    @event = params[2]
+    @amount = params[3]
   end
 
   def get_data
     gh = open('http://data.githubarchive.org')
     js = Zlib::GzipReader.new(gh).read
+    # bq = BigQuery.new(opts)
   end
 
 
