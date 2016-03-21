c53621aac3a7597506a7451b1c317341f911f4a7
Evan Berg <evan.m.berg@gmail.com>
2016-03-21 10:17:23 -0700

    have a breakdown of all the data needed to query

--- gh_archive.rb
+++ gh_archive.rb
@@ -3,8 +3,11 @@ class Archive
   require 'zlib'
   require 'yajl'
   require 'JSON'
-  require 'google-api-client'
-  require 'bigquery'
+  require 'pry'
+  require 'google/api_client'
+  require 'google/api_client/client_secrets'
+  require 'google/api_client/auth/installed_app'
+  # require 'bigquery'
 
   # gz = open('http://data.githubarchive.org/2015-01-01-12.json.gz')
   # js = Zlib::GzipReader.new(gz).read
@@ -22,8 +25,14 @@ class Archive
   def get_input
     puts 'Find the most active repo in a given time frame'
     puts 'example:'
-    puts '2012-11-01, 2012-11-02, PushEvent, 42'
+    puts '2012-11-01 13:00:00, 2012-11-02 15:12:14, PushEvent, 42'
     input = gets.chomp.delete(' ').split(',')
+    @params = []
+    input.each {|x| @params.push(x)}
+    puts @params[0]
+    puts @params[1]
+    puts @params[2]
+    puts @params[3]
   end
 
   def get_data
